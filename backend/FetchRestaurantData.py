18e868fd0602682661870dde2deda513e1e20c81
djs42129 <djsnyder@s172-21-251-h12.paws.uga.edu>
2018-02-10 10:44:33 -0500

    React function

--- backend/FetchRestaurantData.py
+++ backend/FetchRestaurantData.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python2.7
 
-#include sys,os
+import sys,os
+from pyzomato import Pyzomato
+import json
+import requests
+
+textFile = open("output.txt", "w")
 
 
-string APIKey = "ce84c151c3bbfe25dd2c8a29f9da5365"
+APIKey = "ce84c151c3bbfe25dd4c8a29f9da5365"
+
+p = Pyzomato(APIKey)
+
+#print(p.search(q="Athens", cuisines="American", count="50"))
+
+#cityInfo = str(p.getCityDetails(q="Athens"))
+
+#textFile.write(cityInfo)
+#textFile.close()
+
+#with open('data.txt', 'w') as outfile:
+#    json.dump(data, outfile)
+
+#p.getCategories()
+
+
+url = 'https://developers.zomato.com/api/v2.1/locations?'
+payload = open("request.json")
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+r = requests.post(url, headers={'user-key':APIKey, 'query':'Athens' })
+if r.ok:
+	with open('request.json','w') as outfile:
+		json.dump(r.json(),outfile)
---
+++ frontend/ugahacks/src/App/services/ZomatoFetch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+
+const APIKey = "ce84c151c3bbfe25dd4c8a29f9da5365";
+const url = "https://developers.zomato.com/api/v2.1/";
+
+class ZomatoFetch extends Component {
+  
+  function getCityID(name,lon,lat){
+
+  } 
+  render() {
+    <Button color="primary" onClick="getCityID()">
+      Test Functions
+    </Button>
+  }
+}
+
+export default ZomatoFetch;

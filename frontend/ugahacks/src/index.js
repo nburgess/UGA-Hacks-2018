44e3950882d7914cf4021e4c8eef24c1991ce44b
djs42129 <djsnyder@s172-21-251-h12.paws.uga.edu>
2018-02-10 17:05:32 -0500

    Working lat,long

--- frontend/ugahacks/src/index.js
+++ frontend/ugahacks/src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import scriptLoader from 'react-async-script-loader';
 
+//scriptLoader(['https://maps.googleapis.com/maps/api/js?key=AIzaSyAYbtCPuL2LBozcTv1mc2fvKOmAPqy5aqM'])
 render((
     <BrowserRouter>
       <App />

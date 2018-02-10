35c8d5319b30b1a51e4d255eed7c057bdc4fa63b
brentjrobinson <brent_robinson@hotmail.com>
2018-02-10 18:05:39 -0500

    zomato api attempt

--- frontend/ugahacks/src/App/services/ZomatoFetch.js
+++ frontend/ugahacks/src/App/services/ZomatoFetch.js
@@ -18,62 +18,25 @@ class ZomatoFetch extends Component {
     };
     this.getCityID = this.getCityID.bind(this);
   }
-  zomatoCall () {
-
+   
+   getCityID(){
+    fetchNearbyRestaurants = (latitude, longitude) => {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('user-key', 'ce84c151c3bbfe25dd4c8a29f9da5365');
+    let options = {
+      method: 'GET',
+      headers: headers
+    };
+    let url = "https://developers.zomato.com/api/v2.1/geocode?lat=" + latitude + "&lon=" + longitude;
+    window.fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.nearby_restaurants);
+    })
   }
-
-  getCityID () {
-    //input parameters
-    let location = this.state.location; //this.props.location
-    let latitude = this.state.lat;
-    let longitude = this.state.lon;
-    let apiResponse = this.state.apiResponse;
-
-    // var client = zomato.createClient({
-    //   userKey: APIKey, //as obtained from [Zomato API](https://developers.zomato.com/apis) 
-    // });
-
-    // client.getCities({
-    //   q:location, //query by city name 
-    //   lat:latitude, //latitude 
-    //   lon:longitude, //longitude 
-    //   count:"1" // number of maximum result to display 
-    // }, function(err, result){
-    //     if(!err){
-    //       console.log(result);
-    //     }else {
-    //       console.log(err);
-    //     }
-    // });
-    
-    //api call - fetch
-    let ourUrl = this.state.url + 'locations?user-key=' + APIKey + '&query=' + location + '&lat=' + latitude + '&lon=' + longitude;
-    //fetch url
-    let zomatoResponse = fetch(ourUrl)
-    //   ,{
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json',
-    //     'Origin': '',
-    //     'user-key': APIKey,
-    //     'Host': ourUrl
-    //   },
-    //   body: JSON.stringify({
-    //     //'user-key': (APIKey),
-    //     'lat': latitude,
-    //     'lon': longitude,
-    //     'query': location
-    //   }),
-    // });
-
-      .then(response => {
-        return response.json();
-      })
-      .then(data =>{
-        //let apiResponse = response.json();
-      })    
-    //output
+   }
+      
     this.setState({
       response: apiResponse
     })
@@ -81,6 +44,7 @@ class ZomatoFetch extends Component {
     console.log(ourUrl);
     console.log('Response: ')
     console.log(zomatoResponse);
+    console.log(fetchNearbyRestaurants);
   }
   render() {
     // function getCityID () {

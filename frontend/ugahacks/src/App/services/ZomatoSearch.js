4db02f3f98de20d8dc548eeffeb76024d2e0d316
brentjrobinson <brent_robinson@hotmail.com>
2018-02-11 01:33:53 -0500

    fixed zomatosearch

--- frontend/ugahacks/src/App/services/ZomatoSearch.js
+++ frontend/ugahacks/src/App/services/ZomatoSearch.js
@@ -67,48 +67,6 @@ class ZomatoSearch extends Component {
 // };
 }
 
-export function getRestaurant(cuisineID, entityID, entityType){
-  let cuisineID = cuisineID;
-  let entityID = entityID;
-  let entityType = entityType;
-
-
-  let latitude = lat;
-  let longitude = long;
-  let loc = location;
-
-  let headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('user-key', 'ce84c151c3bbfe25dd4c8a29f9da5365');
-  let options = {
-    method: 'GET',
-    headers: headers
-  };
-  let ourUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + latitude + "&lon=" + longitude;
-  console.log(ourUrl);
-  let fetchNearbyRestaurants = fetch(ourUrl, options)
-  .then(response => response.json())
-  .then(data => {
-    let cityID = data.location_suggestions[0].city_id;
-    console.log(cityID);
-    let entityID = data.location_suggestions[0].entity_id;
-    console.log(entityID);
-    let entityType = data.location_suggestions[0].entity_type;
-    console.log(entityType);
-
-    //this.setState({cityID:cityID},() => console.log(this.state.cityID));
-  });
-
-};
-
-export default ZomatoSearch;
-
-
-
-
-
-
-
 export function getRestaurant(cuisineID, entityID, entityType, count = 5){
   let cuisine_ID = cuisineID;
   let entity_ID = entityID;
@@ -132,3 +90,12 @@ export function getRestaurant(cuisineID, entityID, entityType, count = 5){
 
     //this.setState({cityID:cityID},() => console.log(this.state.cityID));
   });
+
+export default ZomatoSearch;
+
+
+
+
+
+
+

b07afcbe85b3f413627ab313aa90e2e766aee88b
brentjrobinson <brent_robinson@hotmail.com>
2018-02-10 23:04:57 -0500

    I guess this is working

--- frontend/ugahacks/src/App/services/ZomatoFetch.js
+++ frontend/ugahacks/src/App/services/ZomatoFetch.js
@@ -7,64 +7,65 @@ const APIKey = 'ce84c151c3bbfe25dd4c8a29f9da5365';
 const url = "https://developers.zomato.com/api/v2.1/";
 
 class ZomatoFetch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: 'https://developers.zomato.com/api/v2.1/',
-      location: 'Athens',
-      lat: '33.9519',
-      lon: '-83.3576',
-      apiResponse: '',
-      fetchNearbyRestaurants: '',
-      cityID: '',
-    };
-    this.getCityID = this.getCityID.bind(this);
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       url: 'https://developers.zomato.com/api/v2.1/',
+//       location: 'Athens',
+//       lat: '33.9519',
+//       lon: '-83.3576',
+//       apiResponse: '',
+//       fetchNearbyRestaurants: '',
+//       cityID: '',
+//     };
+//     this.getCityID = this.getCityID.bind(this);
+//   }
    
-   getCityID(){
-        let latitude = this.state.lat;
-        let longitude = this.state.lon;
-        let loc = this.state.location;
+//    getCityID(){
+//         let latitude = this.state.lat;
+//         let longitude = this.state.lon;
+//         let loc = this.state.location;
 
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('user-key', 'ce84c151c3bbfe25dd4c8a29f9da5365');
-        let options = {
-          method: 'GET',
-          headers: headers
-        };
-        let ourUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + latitude + "&lon=" + longitude + "&count=20";
-        console.log(ourUrl);
-        let fetchNearbyRestaurants = fetch(ourUrl, options)
-        .then(response => response.json())
-        .then(data => {
-          let cityID = data.location_suggestions[0].city_id;
-          this.setState({cityID:cityID},() => console.log(this.state.cityID));
-        });
-      //}
-   }      
+//         let headers = new Headers();
+//         headers.append('Content-Type', 'application/json');
+//         headers.append('user-key', 'ce84c151c3bbfe25dd4c8a29f9da5365');
+//         let options = {
+//           method: 'GET',
+//           headers: headers
+//         };
+//         let ourUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + latitude + "&lon=" + longitude + "&count=20";
+//         console.log(ourUrl);
+//         let fetchNearbyRestaurants = fetch(ourUrl, options)
+//         .then(response => response.json())
+//         .then(data => {
+//           let cityID = data.location_suggestions[0].city_id;
+//           this.setState({cityID:cityID},() => console.log(this.state.cityID));
+//         });
+//       //}
+//    }      
     
-    // this.setState({
-    //   response: apiResponse
-    // })
-    // console.log('Response: ')
-    // console.log(zomatoResponse);
-    // console.log(fetchNearbyRestaurants);
-  render() {
-    return(
-      <Button color="primary" onClick={this.getCityID}>
-        Test
-      </Button>
-    )
-  }
-}
+//     // this.setState({
+//     //   response: apiResponse
+//     // })
+//     // console.log('Response: ')
+//     // console.log(zomatoResponse);
+//     // console.log(fetchNearbyRestaurants);
+//   render() {
+//     return(
+//       <Button color="primary" onClick={this.getCityID}>
+//         Test
+//       </Button>
+//     )
+//   }
+// }
 
 
-// ZomatoFetch.propTypes = {
+/// ZomatoFetch.propTypes = {
 //   location: PropTypes.any.isRequired,
 //   lat: PropTypes.any.isRequired,
-//   long: PropTypes.any.isRequired
+//   long: PropTypes.any.isRequireds
 // };
+}
 
 export function getCityID(lat,long,location){
   let latitude = lat;
@@ -78,14 +79,20 @@ export function getCityID(lat,long,location){
     method: 'GET',
     headers: headers
   };
-  let ourUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + latitude + "&lon=" + longitude + "&count=20";
+  let ourUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + loc + "&lat=" + latitude + "&lon=" + longitude;
   console.log(ourUrl);
   let fetchNearbyRestaurants = fetch(ourUrl, options)
   .then(response => response.json())
   .then(data => {
     let cityID = data.location_suggestions[0].city_id;
     console.log(cityID);
+    let entityID = data.location_suggestions[0].entity_id;
+    console.log(entityID);
+    let entityType = data.location_suggestions[0].entity_type;
+    console.log(entityType);
+
     //this.setState({cityID:cityID},() => console.log(this.state.cityID));
   });
 };
+
 export default ZomatoFetch;

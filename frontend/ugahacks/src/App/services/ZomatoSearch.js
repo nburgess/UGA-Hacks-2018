c6b961ee3cefe91d60fbf7e37b18e93c9da16a16
Daniel Snyder <djsnyder0523@gmail.com>
2018-10-15 10:42:15 -0400

    Update ZomatoSearch.js
    Getting rid of API keys

--- frontend/ugahacks/src/App/services/ZomatoSearch.js
+++ frontend/ugahacks/src/App/services/ZomatoSearch.js
@@ -6,6 +6,8 @@ import zomato from 'zomato';
 const APIKey = 'ce84c151c3bbfe25dd4c8a29f9da5365';
 const url = "https://developers.zomato.com/api/v2.1/";
 
+//I will be phasing out this api key soon
+
 class ZomatoSearch extends Component {
   // constructor(props) {
   //   super(props);
